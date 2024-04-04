c1a38bdd5e13fe4f0c9b7fb07a4eaaf0935d45d4
ujjwal-bh <ujjwalbh001@gmail.com>
2024-04-04 23:02:13 +0530

    db-model:user model created and init migration

---
+++ prisma/migrations/20240404173020_init/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `hashedPassword` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `profile` VARCHAR(191) NULL,
+    `age` INTEGER NULL,
+    `gender` ENUM('MALE', 'FEMALE', 'OTHER') NULL,
+    `profession` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updateAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
